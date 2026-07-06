2e2b3132c4e51788bc61afd902a61f50a9c26142
DE41H <sreyash.s@outlook.com>
2026-07-07 01:00:06 +0530

    using uuuidv7 for pk

--- teams/models.py
+++ teams/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from uuid6 import uuid7
 
 # Create your models here.
+
+class Team(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid7, editable=False)
+    name = models.CharField(max_length=128)
+    members = models.ManyToManyField(to='users.CustomUser', through='teams.Membership', related_name='teams')
+
+    def __str__(self):
+        return str(self.id)
+
+class Role(models.TextChoices):
+    OWNER = 'owner'
+    MAINTAINER = 'maintainer'
+    MEMBER = 'member'
+    VIEWER = 'viewer'
+
+class Membership(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid7, editable=False)
+    user = models.ForeignKey(to='users.CustomUser', on_delete=models.CASCADE, related_name='memberships')
+    team = models.ForeignKey(to='teams.Team', on_delete=models.CASCADE, related_name='memberships')
+    role = models.CharField(max_length=15, choices=Role.choices)
+
+    def __str__(self):
+        return str(self.id)
--- users/models.py
+++ users/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from uuid6 import uuid7
 
 # Create your models here.
 
 class CustomUser(AbstractUser):
-    email = models.EmailField(unique=True, blank=False, null=False)
+    id = models.UUIDField(primary_key=True, default=uuid7, editable=False)
+    email = models.EmailField(unique=True)
 
     def __str__(self):
-        return self.username
+        return str(self.id)
