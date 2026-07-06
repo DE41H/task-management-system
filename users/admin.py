9abfc1aacbaafea18feb4846d5a46aefee4bd202
DE41H <sreyash.s@outlook.com>
2026-07-06 23:57:57 +0530

    setup admin panel for CustomUser model

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,12 @@
-from django.contrib import admin
+from django.contrib.admin.decorators import register
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
 # Register your models here.
+
+@register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    list_display = ['email', 'username']
+    add_fieldsets = [
+        [None, {'classes': ['wide'], 'fields': ['username', 'email', 'password1', 'password2']}]
+    ]
--- users/serializers.py
+++ users/serializers.py
@@ -1,17 +1,15 @@
-from django.contrib.auth import get_user_model
 from rest_framework import serializers
-
-User = get_user_model()
+from .models import CustomUser
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(max_length=128, min_length=8, write_only=True)
 
     class Meta:
-        model = User
+        model = CustomUser
         fields = ['email', 'username', 'password']
 
     def create(self, validated_data):
-        return User.objects.create_user(
+        return CustomUser.objects.create_user(
             email=validated_data['email'],
             username=validated_data['username'],
             password=validated_data['password'],
