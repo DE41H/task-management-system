1836a1ad5f9c307e2417cada0dc07bf130844d11
DE41H <sreyash.s@outlook.com>
2026-07-07 01:06:09 +0530

    made memberships unique

--- teams/models.py
+++ teams/models.py
@@ -23,5 +23,10 @@ class Membership(models.Model):
     team = models.ForeignKey(to='teams.Team', on_delete=models.CASCADE, related_name='memberships')
     role = models.CharField(max_length=15, choices=Role.choices)
 
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['user', 'team'], name='unique_user_team_membership')
+        ]
+
     def __str__(self):
         return str(self.id)
