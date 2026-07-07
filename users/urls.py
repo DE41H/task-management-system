4404dcefc7d0072620992925ea82698a1c2d9c38
DE41H <sreyash.s@outlook.com>
2026-07-08 00:10:05 +0530

    renamed path

--- users/urls.py
+++ users/urls.py
@@ -5,7 +5,7 @@ from .views import RegisterView, UserView, ChangePasswordView
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('me/', UserView.as_view(), name='users'),
+    path('me/', UserView.as_view(), name='user'),
     path('password/', ChangePasswordView.as_view(), name='change_password'),
     path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('logout/', TokenBlacklistView.as_view(), name='token_blacklist'),
