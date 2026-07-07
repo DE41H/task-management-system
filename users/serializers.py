61ebdde1fd1a071411ae1e48d7cdba4ac66a3710
DE41H <sreyash.s@outlook.com>
2026-07-07 15:27:54 +0530

    added password changing and user info changing

--- users/serializers.py
+++ users/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import CustomUser
+from django.contrib.auth.password_validation import validate_password
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(max_length=128, min_length=8, write_only=True)
@@ -14,3 +15,23 @@ class RegisterSerializer(serializers.ModelSerializer):
             username=validated_data['username'],
             password=validated_data['password'],
         )
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['email', 'username']
+
+class ChangePasswordSerializer(serializers.Serializer):
+    new_password = serializers.CharField(max_length=128, min_length=8, write_only=True, required=True, validators=[validate_password])
+    old_password = serializers.CharField(max_length=128, min_length=8, write_only=True, required=True)
+
+    def validate(self, attrs):
+        user: CustomUser = self.context['request'].user
+        if not user.check_password(attrs['old_password']):
+            raise serializers.ValidationError({'old_password': 'password is incorrect'})
+        return super().validate(attrs)
+
+    def update(self, instance: CustomUser, validated_data):
+        instance.set_password(validated_data['new_password'])
+        instance.save()
+        return instance
--- users/views.py
+++ users/views.py
@@ -1,9 +1,23 @@
-from rest_framework.generics import CreateAPIView
-from rest_framework.permissions import AllowAny
-from .serializers import RegisterSerializer
+from rest_framework.generics import CreateAPIView, UpdateAPIView, RetrieveUpdateAPIView
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from .serializers import RegisterSerializer, ChangePasswordSerializer, UserSerializer
 
 # Create your views here.
 
 class RegisterView(CreateAPIView):
     serializer_class = RegisterSerializer
     permission_classes = [AllowAny]
+
+class ChangePasswordView(UpdateAPIView):
+    serializer_class = ChangePasswordSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):  # pyright: ignore[reportIncompatibleMethodOverride]
+        return self.request.user
+
+class UserView(RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):  # pyright: ignore[reportIncompatibleMethodOverride]
+        return self.request.user
